d8aa194b6461d2b962c5099b22e2cc4c00d01f70
David Peng <nyeastufo@gmail.com>
2020-12-13 22:45:33 -0500

    Add Zuul API gateway
    Add Zuul API gateway

---
+++ ZuulServer/src/test/java/com/nyl/zuulserver/ZuulserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nyl.zuulserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZuulserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
